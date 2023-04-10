bf75abf90c75fe615018cdd1f1e289f8c5428200
IraGfC <ahaleogden@hotmail.com>
2023-04-10 15:00:03 -0400

    Updated footstep audio

--- Assets/Scripts/footsteps.cs
+++ Assets/Scripts/footsteps.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class footsteps : MonoBehaviour
 {
     public AudioSource footstep1;
+    
 
     //Enables the footstep sound
     //Needs to be reconfigured for controller
