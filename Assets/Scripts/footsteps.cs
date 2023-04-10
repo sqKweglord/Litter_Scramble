85ff60ae7f86013e83d6d699a80e633cfd794d15
IraGfC <ahaleogden@hotmail.com>
2023-04-10 15:05:37 -0400

    Added controller support to footsteps

--- Assets/Scripts/footsteps.cs
+++ Assets/Scripts/footsteps.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class footsteps : MonoBehaviour
 {
     public AudioSource footstep1;
-    
+    Vector3 lastPos;  
+    public GameObject player;  
 
     //Enables the footstep sound
-    //Needs to be reconfigured for controller
     void Update()
     {
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        //Detects player movement
+        if(player.transform.position != lastPos)
         {
             footstep1.enabled = true;
         }
+        
         else
         {
             footstep1.enabled = false;
         }
+        //Resets player's last known location
+        lastPos = player.transform.position;
     }
 }
